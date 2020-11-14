896e3c9dea3b33546b9585a341a3c7774e53f9f3
enes esen <enes.esen@puffytech.com>
2020-11-15 01:54:02 +0300

    fix: nextI18

--- next.config.js
+++ next.config.js
@@ -1,18 +1,13 @@
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
+const { nextI18NextRewrites } = require('next-i18next/rewrites');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
-const localeSubpaths = {
-  tr: 'tr',
-}
+const localeSubpaths = { tr: 'tr' };
 
 module.exports = withBundleAnalyzer({
+  rewrites: async () => nextI18NextRewrites(localeSubpaths),
   publicRuntimeConfig: {
     localeSubpaths,
   },
-
-  async rewrites() {
-    return [...nextI18NextRewrites(localeSubpaths)]
-  },
-})
+});
