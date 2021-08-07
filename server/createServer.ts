1ed044285fe76124ed76999d0145dfe6c18aaa17
Enes Esen <enesen@gmx.com>
2021-08-07 16:48:55 +0300

    feat: added sentry

--- server/createServer.ts
+++ server/createServer.ts
@@ -4,7 +4,7 @@ import { ServerRegistration } from 'apollo-server-micro/dist/ApolloServer'
 import { createContext } from './context'
 import { schema } from './schema'
 
-export async function createServer (params?: ServerRegistration) {
+export async function createServer(params?: ServerRegistration) {
   const apolloServer = new ApolloServer({
     schema,
     context: createContext,
--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -2,16 +2,17 @@ import { NextApiRequest, NextApiResponse, PageConfig } from 'next'
 
 import Cors from 'cors'
 import { createServer } from '../../../server/createServer'
-import initMiddleware  from '../../../server/utils/initMiddleware'
+import initMiddleware from '../../../server/utils/initMiddleware'
+import { withSentry } from '@sentry/nextjs'
 
 // Initialize the cors middleware
 const cors = initMiddleware(
   // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
     // Only allow requests with GET, POST and OPTIONS
-    methods: ["GET", "POST", "OPTIONS"],
+    methods: ['GET', 'POST', 'OPTIONS'],
   })
-);
+)
 
 export const config: PageConfig = {
   api: {
@@ -19,17 +20,11 @@ export const config: PageConfig = {
   },
 }
 
-export default async function handler(req:NextApiRequest, res:NextApiResponse):Promise<void> {
+async function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
   await cors(req, res)
   res.setHeader('Access-Control-Allow-Credentials', 'true')
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://studio.apollographql.com'
-  )
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
+  res.setHeader('Access-Control-Allow-Origin', 'https://studio.apollographql.com')
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   if (req.method === 'OPTIONS') {
     res.end()
     return
@@ -37,6 +32,7 @@ export default async function handler(req:NextApiRequest, res:NextApiResponse):P
   const apolloServerHandler = await createServer({
     path: '/api/graphql',
   })
-  return apolloServerHandler(req, res);
+  return apolloServerHandler(req, res)
 }
 
+export default withSentry(handler)
