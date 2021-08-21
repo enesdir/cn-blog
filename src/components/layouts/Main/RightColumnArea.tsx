963846532f9bc9c176875b7b61dbd90c15c7e940
Enes Esen <enesen@gmx.com>
2021-08-21 13:43:34 +0300

    fix: dark mode

--- src/components/layouts/Main/RightColumnArea.tsx
+++ src/components/layouts/Main/RightColumnArea.tsx
@@ -3,13 +3,13 @@ import Link from '@material-ui/core/Link'
 import Paper from '@material-ui/core/Paper'
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
+import { grey } from '@material-ui/core/colors'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles(theme => ({
   rightColumnAboutBox: {
     padding: theme.spacing(2),
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    backgroundColor: theme.palette.type === 'light' ? grey[200] : grey[800],
   },
   rightColumnSection: {
     marginTop: theme.spacing(3),
